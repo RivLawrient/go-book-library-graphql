5bc3afed0f5dd6d358f5be9aa9b5c7f3eb0eff9b
lawrient <sandincl.d@gmail.com>
2025-02-15 02:32:52 +0800

    handle graphql

--- main.go
+++ main.go
@@ -2,18 +2,16 @@ package main
 
 import (
 	"fmt"
-	"go-book-library-graphql/internal/config"
+	"go-book-library-graphql/internal/graph"
 	"go-book-library-graphql/internal/handlers"
 	"log"
 	"net/http"
 )
 
 func main() {
-	db := config.GetConnection()
-	reg := &config.RegisterConfig{
-		Db: db,
-	}
-	http.Handle("/graphql", handlers.GraphQLHandler(config.Register(reg)))
+	http.Handle("/graphql", handlers.GraphQLHandler(
+		graph.Schema(),
+	))
 
 	fmt.Println("🚀 Server berjalan di http://localhost:8080/graphql")
 	log.Fatal(http.ListenAndServe(":8080", nil))
